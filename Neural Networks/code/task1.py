464f1888b5fa2c2232b16391ec3b9aebeb75681c
Precioux <uni.mahdipour@gmail.com>
2023-05-23 14:54:51 +0330

    Neural Networks- task 1 - implementing model

--- Neural Networks/code/task1.py
+++ Neural Networks/code/task1.py
@@ -1,5 +1,12 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
+from layers.convolution2d import Conv2D
+from layers.maxpooling2d import MaxPool2D
+from layers.fullyconnected import FC
+from activations import Activation, get_activation
+from losses.binarycrossentropy import BinaryCrossEntropy
+from losses.meansquareerror import MeanSquaredError
+from model import Model
 
 # Step 1: Load the dataset
 train_data = pd.read_csv('C:/Users/Samin/Desktop/University/Term 7/Computational Intelligence/Projects/Neural Networks/code/datasets/california_houses_price/california_housing_train.csv')
@@ -24,3 +31,26 @@ X_test_scaled = scaler.transform(X_test)
 # Step 5: Verify the preprocessed data
 print(X_train_scaled[:5])
 print(X_test_scaled[:5])
+
+# Step 6: Define the architecture
+arch = {
+    'conv1': Conv2D(in_channels=3, out_channels=16, kernel_size=3, stride=1),
+    'pool1': MaxPool2D(kernel_size=2, stride=2),
+    'conv2': Conv2D(in_channels=16, out_channels=32, kernel_size=3, stride=1),
+    'pool2': MaxPool2D(kernel_size=2, stride=2),
+    'fc1': FC(in_features=32 * 6 * 6, out_features=120),
+    'fc2': FC(in_features=120, out_features=84),
+    'fc3': FC(in_features=84, out_features=10),
+    'activation': get_activation('relu')
+}
+
+# Step 7: Create the criterion (loss) function
+criterion = BinaryCrossEntropy()  # or MeanSquaredError()
+
+# Step 8: Create the model instance
+model = Model(arch, criterion)
+
+# Step 9: Train the model
+epochs = 10  # Set the number of epochs
+batch_size = 32  # Set the batch size
+model.train(X_train_scaled, y_train, epochs, val=None, batch_size=batch_size, shuffling=False, verbose=1)
