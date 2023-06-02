116c45b1d889cf8ef9bbfde6f4c3d342e16e2029
Precioux <uni.mahdipour@gmail.com>
2023-06-02 14:24:31 +0330

    Fuzzy - starting

--- Fuzzy/CI_PRJ_Fuzzy/FuzzySelfDriving/fuzzy_controller.py
+++ Fuzzy/CI_PRJ_Fuzzy/FuzzySelfDriving/fuzzy_controller.py
@@ -1,16 +1,27 @@
 class FuzzyController:
-    """
-    #todo
-    write all the fuzzify,inference,defuzzify method in this class
-    """
+    def __init__(self, left_dist, center_dist, right_dist):
+        self.left_dist = left_dist
+        self.center_dist = center_dist
+        self.right_dist = right_dist
 
-    def __init__(self):
-        pass
+    def fuzzify_left_dist(self):
+        # Fuzzify the left distance value using the fuzzy chart
+        # Map the left distance to linguistic terms (e.g., 'close', 'medium', 'far')
+        # Return the fuzzy values for each linguistic term
 
+    def fuzzify_center_dist(self):
+        # Fuzzify the center distance value using the fuzzy chart
+        # Map the center distance to linguistic terms
+        # Return the fuzzy values for each linguistic term
 
-    def decide(self, left_dist,right_dist):
-        """
-        main method for doin all the phases and returning the final answer for rotation
-        """
+    def fuzzify_right_dist(self):
+        # Fuzzify the right distance value using the fuzzy chart
+        # Map the right distance to linguistic terms
+        # Return the fuzzy values for each linguistic term
+
+    def decide(self):
+        # Call the fuzzification methods to obtain the fuzzy values for each input variable
+        # Perform inference using fuzzy rules
+        # Defuzzify the output variable
+        # Return the final answer for rotation
         return 0
-    
